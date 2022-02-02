102e544e26918a28ceae563dea0f76f8df6b54c3
sushilwaghe <sushilwaghe2169@gmail.com>
2022-02-03 02:31:38 +0530

    added progressbar

--- js/script.js
+++ js/script.js
@@ -10,17 +10,22 @@ function calculateBMI() {
 
     if(bmi < 18.5){
         document.getElementById("txt-remark").innerHTML = "Underweight";
+        document.getElementById("progress-remark").className = "progress-bar progress-bar-striped progress-bar-animated";
     }
     else if(bmi >= 18.5 && bmi <= 24.9){
         document.getElementById("txt-remark").innerHTML = "Normal";
+        document.getElementById("progress-remark").className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
     }
     else if(bmi >= 25 && bmi <= 29.9){
         document.getElementById("txt-remark").innerHTML = "Overweight";
+        document.getElementById("progress-remark").className = "progress-bar progress-bar-striped progress-bar-animated bg-info";
     }
     else if(bmi >= 30 && bmi <= 34.9){
         document.getElementById("txt-remark").innerHTML = "Obese";
+        document.getElementById("progress-remark").className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
     }
     else if(bmi >= 34.9 && bmi <= 40){
         document.getElementById("txt-remark").innerHTML = "Extremely Overweight ";
+        document.getElementById("progress-remark").className = "progress-bar progress-bar-striped progress-bar-animated bg-danger";
     }
 }
